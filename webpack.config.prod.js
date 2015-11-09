a7a5dbc146e0caccf1a0e1d586ca47ad2e988d2f
Julian Scheid <julians37@gmail.com>
2015-11-09 23:58:40 +0100

    Production deployment

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,17 +1,22 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var AssetsPlugin = require('assets-webpack-plugin');
+var assetsPluginInstance = new AssetsPlugin();
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    './src/index'
-  ],
+  entry: {
+    main: './src/index',
+    worker: './src/worker',
+  },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist', 'static'),
+    filename: '[name]_[chunkhash].js',
     publicPath: '/static/'
   },
   plugins: [
+    new ExtractTextPlugin('style_[chunkhash].css', { allChunks: true }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
@@ -22,13 +27,33 @@ module.exports = {
       compressor: {
         warnings: false
       }
-    })
+    }),
+    assetsPluginInstance,
+    new HtmlWebpackPlugin({
+      title: 'Nim Sandbox',
+      template: 'prod_index.html',
+      favicon: 'static/favicon.ico',
+      filename: '../index.html',
+    }),
   ],
+  resolve: {
+    modulesDirectories: ['', 'node_modules'],
+    extensions: [ '', '.js', '.jsx', '.css', '.scss'  ],
+  },
+  node: {
+    fs: 'empty',
+  },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'src'),
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style', 'css!postcss'),
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass'),
     }]
   }
 };
